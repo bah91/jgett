10903948e7c4b879af481c5b32870efeb3e107c8
Gian Luca Dalla Torre <g.dallatorre@atcetera.it>
2012-02-16 13:53:45 +0100

    Add user information interface

---
+++ src/test/java/it/atcetera/jgett/JGettClientTest.java
@@ -0,0 +1,22 @@
+package it.atcetera.jgett;
+
+import org.testng.annotations.Test;
+
+/**
+ * Test Case for JGett Client
+ * 
+ * @author Gian Luca Dalla Torre <g.dallatorre@atcetera.it>
+ * @version $Id$
+ *
+ */
+public class JGettClientTest {
+	
+	/**
+	 * Test for client authentication
+	 */
+	@Test
+	public void testAuthentication(){
+		
+	}
+
+}
